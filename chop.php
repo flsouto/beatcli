297362ba623bfb1129da13dc601cfd2bbbea95af
flsouto <fabiolimasouto@gmail.com>
2023-12-29 15:14:40 -0300

    updates

--- chop.php
+++ chop.php
@@ -4,7 +4,7 @@ require('utils.php');
 
 shell_exec("rm chop -Rf 2>/dev/null; mkdir chop");
 
-$s = sampler($argv[1]);
+$s = sampler($argv[1]??'.ytdl/out.wav');
 foreach($s->split(ceil($s->len()/60)) as $i => $p){
     echo "At $i\n";
     $p->save("chop/$i.wav");
