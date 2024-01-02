b5ac922b5ac2e0d890d8559d801e8ed766ea0a0e
flsouto <fabiolimasouto@gmail.com>
2024-01-02 13:45:48 -0300

    updates

--- mixers/halfway3.php
+++ mixers/halfway3.php
@@ -6,6 +6,16 @@ return function($a,$b,$c){
     $c->resize($a->len());
     $afout = $a()->fade(0,-15);
     $p1 = $afout()->mix($b()->fade(-15,0));
-    $p2 = $afout()->mix($c()->fade(-15,0));
-    return $p1->add($p2)->x(4);
+    $p2 = $afout();
+    if(mt_rand(0,1)){
+        $p2->mod('pitch -'.mt_rand(40,80));
+    }
+    $p2->mix($c()->fade(-15,0));
+    $out = $p1->add($p2);
+    if($out->len() <= 9){
+        $out->x(4);
+    } else {
+        $out->x(2);
+    }
+    return $out;
 };
