6f92fe943f9a3faf6baccb31c0d052ec9886d338
flsouto <fabiolimasouto@gmail.com>
2024-01-26 18:04:13 -0300

    updates

---
+++ mixers/adrenaline.php
@@ -0,0 +1,13 @@
+<?php
+
+return function($a){
+    if(mt_rand(0,1)) $a->mod('tempo .'.mt_rand(8,9));
+    $len = mt_rand(9,20);
+    $a->resize($len);
+    first_segment($a);
+    $len = $a->len();
+    $shadow = $a::silence(mt_rand(1,6)/1000)->add($a);
+    if(mt_rand(0,1)) $shadow->mod('tempo .9'.mt_rand(8,9));
+    if(mt_rand(0,1)) $shadow->tremolo('.'.mt_rand(1,9).' 99');
+    return $a->mix($shadow,false)->cut(0,$len)->x(4);
+};
