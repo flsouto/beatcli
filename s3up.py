2f65683facd6c4a73d3b2df4ed840c4ae4adb8b0
flsouto <flsouto666@gmail.com>
2024-07-20 14:06:13 -0300

    updates

--- s3up.py
+++ s3up.py
@@ -2,12 +2,18 @@ from utils import *
 import glob
 import os
 import random
+uploaded = Object('data/uploaded.json')
+basename = os.path.basename
 
 globs = os.getenv('S3UPGLOB').split(';')
-files = [f for g in globs for f in glob.glob(g)]
+files = [f for g in globs for f in glob.glob(g) if basename(f) not in uploaded]
 random.shuffle(files)
-print(files)
-
-uploaded = Object('data/uploaded.json')
-uploaded['test'] = 1
-uploaded.save(indent=1)
+len = len(files)
+i=1
+for f in files:
+	print(f'At {i} of {len}')
+	publish_file(f, basename(f))
+	print(file_url(basename(f)))
+	uploaded[basename(f)] = 1
+	uploaded.save(indent=1)
+	i+=1
