8888b04f4d751758fa1b54e348809fb847165b90
flsouto <fabiolimasouto@gmail.com>
2025-02-06 13:48:18 -0300

    updates

--- cloud.py
+++ cloud.py
@@ -5,6 +5,7 @@ import mimetypes
 
 def get_s3() :
     if not hasattr(get_s3, 'cache'):
+        print(os.getenv('AWS_KEY'))
         get_s3.cache = boto3.client('s3',
             aws_access_key_id = os.getenv('AWS_KEY'),
             aws_secret_access_key = os.getenv('AWS_SECRET'),
